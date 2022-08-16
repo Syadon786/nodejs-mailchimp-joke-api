636be11226389925ea8c1020b5b8fb156b9c7849
Gábor Bóka <bokagabor4@gmail.com>
2022-08-16 17:48:16 +0200

    get joke from jokeapi and create test campaign

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
 const express = require('express');
 const app = express();
+
 const bodyParser = require('body-parser');
 const jsonfile = require('jsonfile')
 
+const https = require('https');
+
+//Mailchimp api setup
 const mailchimp = require("@mailchimp/mailchimp_marketing");
-
-const apiKey = jsonfile.readFileSync(__dirname + "/apikey.json").apiKey;
-
+const mailChimpApiKey = jsonfile.readFileSync(__dirname + "/apikey.json").mailChimpApiKey;
 mailchimp.setConfig({
-    apiKey: apiKey,
+    apiKey: mailChimpApiKey,
     server: "us18",
   });
 const listId = "fc8da8e711";
+
+//JokeApi api setup
+const jokeApiUrl = "https://v2.jokeapi.dev/joke/Dark?";
+const jokeApiParameters = {
+    amount: 1
+};
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname)); //css miatt
 
 const server = app.listen(process.env.PORT || 8000, () => {  
     console.log(`Server is running on port ${server.address().port}.`); 
-      pingMailChimpServer();
+    pingMailChimpServer();
 });
 
 app.get('/', (req, res) => {
-    console.log("Csatlakoztak a szerverre");
-    res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get('/jokes', (req, res) => {
+    sendMail(res);
 });
 
 app.post('/', (req, res) => {
@@ -35,6 +46,33 @@ app.post('/', (req, res) => {
 app.post('/failure', (req, res) => {
     res.redirect('/');
 });
+
+
+function sendMail(res) {
+   getJoke()
+  .then((joke) => {
+      //console.log(joke);
+      createCampaign(joke);
+      res.send(joke);
+    }) ;
+}
+
+//! Get a joke with jokeapi
+function getJoke() {
+  return new Promise((resolve, reject) =>  {
+      https.get(`${jokeApiUrl}${Object.entries(jokeApiParameters)[0].join('=')}`, (response) => {
+      console.log(response.statusCode);
+      response.on('data', (data) => {       
+            try {
+               const joke = JSON.parse(data);
+               resolve(joke);
+            } 
+            catch(error) {
+              reject(error);
+          }      
+      });
+  }) });
+}
 
 //!https://mailchimp.com/developer/marketing/guides/create-your-first-audience/#add-a-contact-to-an-audience
 async function addContactMember(subscribingUser, res) {
@@ -50,7 +88,7 @@ async function addContactMember(subscribingUser, res) {
     }); 
   }
   catch (error) {
-     console.log("Nem volt sikeres");
+     console.log(error);
      res.sendFile(__dirname + "/failure.html");
      return;
   }; 
@@ -62,5 +100,27 @@ async function addContactMember(subscribingUser, res) {
 
 async function pingMailChimpServer() {
     const response = await mailchimp.ping.get();
-        console.log(response);
+    console.log(response);
 }
+
+async function createCampaign(joke) {
+  const response = await mailchimp.campaigns.create({ 
+                            type: "plaintext" , 
+                            recipients: { 
+                                list_id : listId  
+                            },
+                            settings: {
+                               subject_line: "Joke of the day",
+                               title: "Joke of the day",
+                               from_name: "Syadon",
+                               reply_to: "bokagabor4@gmail.com"
+                            }
+                            });
+                
+  console.log(response);
+}
+
+/*async function getListMemberEmails()  {   //filter csak e-mail info lekérdezésére fields: ["members.email_address"]
+  const response = await mailchimp.lists.getListMembersInfo(listId, {fields: ["members.email_address"]});
+  return await response.members;
+}; */
